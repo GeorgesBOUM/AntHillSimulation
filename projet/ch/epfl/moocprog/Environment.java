3163abab9a7454d0ea7173fc47bca427c52bdb56
GeorgesBOUM <gregoire.maison@icloud.com>
2020-10-30 17:19:15 +0100

    mise à jour de l'Environnement

--- projet/ch/epfl/moocprog/Environment.java
+++ projet/ch/epfl/moocprog/Environment.java
@@ -4,9 +4,13 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
 
+import ch.epfl.moocprog.gfx.EnvironmentRenderer;
 import ch.epfl.moocprog.utils.Time;
 
 import static ch.epfl.moocprog.utils.Utils.requireNonNull;
+import static ch.epfl.moocprog.config.Config.WORLD_HEIGHT;
+import static ch.epfl.moocprog.config.Config.WORLD_WIDTH;
+import static ch.epfl.moocprog.app.Context.getConfig;
 
 /**
  * Modélise l'environnement dans lequel évolue les différentes entités
@@ -49,5 +53,45 @@ public final class Environment implements FoodGeneratorEnvironmentView{
 	public void update(Time dt) {
 		this.foodGenerator.update(this, dt);
 		this.listFood.removeIf(food -> food.getQuantity() <= 0);
+	}
+	
+	/**
+	 * Permet le rendu graphique des éléments de l'environnement
+	 * @param environmentRenderer
+	 */
+	public void renderEntities(EnvironmentRenderer environmentRenderer) {
+		this.listFood.forEach(environmentRenderer::renderFood);
+	}
+	
+	/**
+	 * Ajoute une fourmilière dans l'environnement
+	 * @param a
+	 */
+	public void addAnthill(Anthill anthill) {
+		
+	}
+	
+	/**
+	 * Ajoute un animal dans l'environnement
+	 * @param animal
+	 */
+	public void addAnimal(Animal animal) {
+		
+	}
+	
+	/**
+	 * Retourne la largeur de l'environnement
+	 * @return la largeur de l'environnement
+	 */
+	public int getWidth() {
+		return getConfig().getInt(WORLD_WIDTH);
+	}
+	
+	/**
+	 * Retourne la hauteur de l'environnement
+	 * @return la hauteur de l'environnement
+	 */
+	public int getHeight() {
+		return getConfig().getInt(WORLD_HEIGHT);
 	}
 }
